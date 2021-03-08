c9180e56bfb133eb4565f9bb803ecb3d89782f14
Daniil Smahtin <dansmht16@gmail.com>
2021-03-08 12:41:39 +0300

    add button for reincarnation

--- src/components/ShopBlock/ShopBlock.vue
+++ src/components/ShopBlock/ShopBlock.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="shop-block">
     <div class="scroll-wrapper">
-      Shop
+      <ul class="shop-list">
+        <li class="reincarnate-button">
+          Reincarnate (you will receive 10 material sapphires)
+        </li>
+      </ul>
     </div>
   </div>
 </template>
